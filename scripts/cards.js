6df83c47b4a44340d394e947049297805f66f096
melkiurak <melkiurak3209@gmail.com>
2025-05-29 12:08:30 +0300

    Add cart for table

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,15 +2,14 @@ const suits = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
 const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 const cards = [];
 const players = 2;
-
 for(const suit of suits){
     for(const value of values){
         cards.push({suit, value})
     };
 };
-function randomCard(){
+function randomCard(count){
     let cardsPlayer = [];
-    for(let i = 0; i < 2; i++){
+    for(let i = 0; i < count; i++){
         const randomIndex = Math.floor(Math.random() * cards.length);
         cardsPlayer.push(cards[randomIndex]);
         cards.splice(randomIndex, 1);
@@ -18,8 +17,10 @@ function randomCard(){
     return cardsPlayer;
 }
 const playersCards = [];
-
 for(let i = 0; i < players; i++){
-    playersCards.push(randomCard())
+    playersCards.push(randomCard(2));
 }
+const tableCards = randomCard(5);
+
 console.log(playersCards)
+console.log(tableCards)
