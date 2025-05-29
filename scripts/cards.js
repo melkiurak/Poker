5800717463486f8f684f5277ba24c84140d2dfe6
melkiurak <melkiurak3209@gmail.com>
2025-05-29 11:43:02 +0300

    Add random

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,5 +1,19 @@
-const suit = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
-const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A',]
+const suits = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
+const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 const cards = [];
 
-console.log('Я карточка')
+for(const suit of suits){
+    for(const value of values){
+        cards.push({suit, value})
+    };
+};
+function randomCard(){
+    let cardsPlayer = [];
+    for(let i=0; i<2; i++){
+        const randomIndex = Math.floor(Math.random() * cards.length)
+        cardsPlayer.push(randomIndex)
+        cardsPlayer.splice(randomIndex, 1);
+    }
+    return cardsPlayer
+}
+console.log(randomCard())
