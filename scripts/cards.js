3d305e961e32000c7a5ba5e51c74ca66626ab3be
melkiurak <melkiurak3209@gmail.com>
2025-05-29 13:19:22 +0300

    Add cart and text by table

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,7 +1,9 @@
+const cardContainer = document.querySelector('.table__area-cards')
 const suits = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
 const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 const cards = [];
 const players = 2;
+
 for(const suit of suits){
     for(const value of values){
         cards.push({suit, value})
@@ -22,5 +24,36 @@ for(let i = 0; i < players; i++){
 }
 const tableCards = randomCard(5);
 
+function cardToString(card){
+    const suitSymbols = {
+        Diamonds: '/asset/Diamonds.png',
+        Clubs: '/asset/Club.png',
+        Spades: '/asset/Spades.png',
+        Hearts: '/asset/Heart.png',
+    };
+    return suitSymbols[card.suit]
+}
+const colorSuit = {
+    Diamonds: '#EAA680',
+    Clubs: '#7AA7FF',
+    Spades: '#98DEE3',
+    Hearts: '#E97B88',
+}
 console.log(playersCards)
 console.log(tableCards)
+
+tableCards.forEach((card) => {
+    const cardElement = document.createElement('div');
+    const suitImg = document.createElement('img');
+    const cardValue = document.createElement('h2');
+
+    suitImg.src = cardToString(card);
+    cardElement.classList.add('card');
+    cardValue.textContent = card.value;
+    cardValue.style.color = colorSuit[card.suit]
+
+    
+    cardContainer.appendChild(cardElement);
+    cardElement.appendChild(suitImg);
+    cardElement.appendChild(cardValue);
+})
