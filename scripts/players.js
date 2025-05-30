4a74f3f786383316facc60122ded068276415c1b
melkiurak <melkiurak3209@gmail.com>
2025-05-30 12:16:30 +0300

    Add players

---
+++ scripts/players.js
@@ -0,0 +1,56 @@
+const playerWrapper = document.getElementById('player__wrapper');
+const btnAddPlayer = document.getElementById('addPlayer');
+const avatarList = [
+    '/asset/Players/Avatar.png',
+    '/asset/Players/Avatar2.png',
+    '/asset/Players/Avatar3.png',
+    '/asset/Players/Avatar4.png',
+    '/asset/Players/Avatar5.png',
+]
+const nameList = ["Jack","Max","Leo","Cody","Ace","Duke","Chase","Finn","Blake"]
+
+let countPlayer = 0
+let maxPlayer = 9
+
+
+
+function randomAvatar()  {
+    const avatarIndex = Math.floor(Math.random() * avatarList.length)
+    return avatarList[avatarIndex]
+}
+function randomName() {
+    const nameIndex = Math.floor(Math.random() * nameList.length)
+    return nameList[nameIndex]
+}
+function createPlayer() {
+    const avatar = randomAvatar();
+    const name = randomName();
+    const player = document.createElement('div');
+    player.className = 'player'
+    player.innerHTML = ` <div class="player__timer"></div>
+        <div class="player__info">
+            <img class="player__info-img" src="${avatar}" alt="">
+            <div class="player__info-amount">
+                <span>Amount</span>
+                <img src="./asset/Players/amoutIMG.png" alt="">
+            </div>
+            <div class="player__info-name">
+                <span>${name}</span>
+            </div>
+        </div>
+    `
+    playerWrapper.appendChild(player)
+};
+
+
+btnAddPlayer.addEventListener('click', () => {
+    if(countPlayer < maxPlayer){
+        countPlayer++;
+        createPlayer();
+    }
+    if(countPlayer === maxPlayer) {
+        btnAddPlayer.disabled  =  true;
+        btnAddPlayer.style.display = 'none'
+        alert('Maxt PLayers')
+    }
+})
