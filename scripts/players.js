09e8927cb044b028af9cf3e7f166c2257aa7e088
melkiurak <melkiurak3209@gmail.com>
2025-05-31 13:16:09 +0300

    Add check player

--- scripts/players.js
+++ scripts/players.js
@@ -17,10 +17,39 @@ let maxPlayer = 9
 function randomAvatar()  {
     const avatarIndex = Math.floor(Math.random() * avatarList.length)
     return avatarList[avatarIndex]
-}
+};
 function randomName() {
     const nameIndex = Math.floor(Math.random() * nameList.length)
     return nameList[nameIndex]
+};
+function postitonPlayer(player, index, total) {
+    if(index === 0) {
+        player.style.left = '50%';
+        player.style.bottom = '-40px';
+        player.style.transform = 'translateX(-50%)';
+    }
+    if(total === 2){
+        const positions = [
+            { left: '50%', bottom: '-40px', transform: 'translateX(-50%)' },
+            { left: '10%', top: '0'},
+            { right: '10%', top: '0'},
+        ];
+        player.style.left = positions[index].left || '';
+        player.style.right = positions[index].right || '';
+        player.style.bottom = positions[index].bottom || '';
+        player.style.top = positions[index].top || '';
+        player.style.transform = positions[index].transform;
+    }  
+    if(total === 1){
+          const positions = [
+            { left: '50%', bottom: '-40px', transform: 'translateX(-50%)' },
+            { left: '50%', top: '-40px', transform: 'translateX(-50%)' },
+        ];
+        player.style.left = positions[index].left || '';
+        player.style.bottom = positions[index].bottom || '';
+        player.style.top = positions[index].top || '';
+        player.style.transform = positions[index].transform || '';
+    }
 }
 function createPlayer() {
     const avatar = randomAvatar();
@@ -44,9 +73,8 @@ function createPlayer() {
         </div>
     `
     playerWrapper.appendChild(player)
+    postitonPlayer(player, countPlayer - 1, maxPlayer)
 };
-
-
 btnAddPlayer.addEventListener('click', () => {
     if(countPlayer < maxPlayer){
         countPlayer++;
