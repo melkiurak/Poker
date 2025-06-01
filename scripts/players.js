83f77401f14becc11f4959727c3c9623363fdcdd
melkiurak <melkiurak3209@gmail.com>
2025-06-01 13:24:52 +0300

    Add position players

--- scripts/players.js
+++ scripts/players.js
@@ -8,11 +8,71 @@ const avatarList = [
     '/asset/Players/Avatar5.png',
 ]
 const nameList = ["Jack","Max","Leo","Cody","Ace","Duke","Chase","Finn","Blake"]
-const positions = [
-  { left: '50%', bottom: '-40px', transform: 'translateX(-50%)' },
-  { left: '20%', top: '10px' },
-  { left: '80%', top: '10px' }
-]
+const playerPositions = {
+    1: {
+       0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' },
+    },
+    2: {
+        0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' },
+        1: { left: '50%', top: '-25px', transform: 'translateX(-50%)' }    
+    },
+    3: {
+        0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' }, 
+        1: { left: '14%', top: '0px' },                                   
+        2: { right: '14%', top: '0px' }                                   
+    },
+    4: {
+        0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' }, 
+        1: { left: '-10%', top: '50%', transform: 'translateY(-50%)' },  
+        2: { left: '50%', top: '-25px', transform: 'translateX(-50%)' }, 
+        3: { right: '-10%', top: '50%', transform: 'translateY(-50%)' } 
+    },
+    5: {
+        0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' },
+        1: { left: '-4%', bottom: '19%' },
+        2: { left: '-4%', top: '19%' },
+        3: { right: '-4%', top: '19%' },
+        4: { right: '-4%', bottom: '19%' }
+    },
+    6: {
+        0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' },
+        1: { left: '-4%', bottom: '19%' },
+        2: { left: '-4%', top: '19%' },
+        3: { left: '50%', top: '-25px', transform: 'translateX(-50%)' },
+        4: { right: '-4%', top: '19%' },
+        5: { right: '-4%', bottom: '19%' }
+    },
+    7: {
+        0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' },
+        1: { left: '-4%', bottom: '19%' },
+        2: { left: '-4%', top: '19%' },
+        3: { left: '26%', top: '-5px', transform: 'translateX(-25%)' },
+        4: { right: '21%', top: '-5px' },
+        5: { right: '-4%', top: '19%' },
+        6: { right: '-4%', bottom: '19%' }
+    },
+    8: {
+        0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' },
+        1: { left: '5%', bottom: '10%' },
+        2: { left: '-10%', top: '50%', transform: 'translateY(-50%)' },
+        3: { left: '5%', top: '10%' },
+        4: { left: '50%', top: '-25px', transform: 'translateX(-50%)' },
+        5: { right: '5%', top: '10%' },
+        6: { right: '-10%', top: '50%', transform: 'translateY(-50%)' },
+        7: { right: '5%', bottom: '10%' }
+    },
+    9: {
+        0: { left: '50%', bottom: '-25px', transform: 'translateX(-50%)' },
+        1: { left: '5%', bottom: '10%' },
+        2: { left: '-10%', top: '50%' },
+        3: { left: '0%', top: '20%' },
+        4: { left: '24%', top: '0%' },
+        5: { right: '25%', top: '0%' },
+        6: { right: '0%', top: '20%' },
+        7: { top: '50%', right: '-10%' },
+        8: { right: '5%', bottom: '10%' }
+    }
+};
 
 
 let countPlayer = 0
@@ -31,179 +91,15 @@ function postitonPlayer(player, index, total) {
     player.style.left = '';
     player.style.top = '';
     player.style.bottom = '';
+    player.style.right = '';
     player.style.transform = '';
 
-    switch(index){
-        case 0: 
-            player.style.left = '50%';
-            player.style.bottom = '-25px';
-            player.style.transform = 'translateX(-50%)';
-            break;
-        case 1:
-            if(total === 8 || total === 9) {
-                player.style.left = '5%';
-                player.style.bottom = '10%';
-            } else if(total === 5 || total === 6 || total === 7) {
-                player.style.left = '-4%';
-                player.style.bottom = '19%';
-                player.style.transform = ''
-            }
-            else if(total === 4){
-                player.style.left = '-10%';
-                player.style.top = '50%';
-                player.style.transform = 'translateY(-50%)'
-            }
-            else if(total === 3){
-                player.style.left = '14%';
-                player.style.top = '0px';
-            }
-             else if(total === 2) {
-                player.style.left = '50%';
-                player.style.transform = 'translateX(-50%)';
-                player.style.top = '-25px';
-            }
-            break;
-        case 2: 
-            if(total === 9) {
-                player.style.left = '-10%';
-                player.style.transform = ''
-                player.style.top = '50%';
-                player.style.right = '';
-            } else if(total === 8){
-                player.style.left = '-10%';
-                player.style.transform = 'translateY(-50%)';
-                player.style.top = '50%';
-                player.style.right = '';
-            } 
-            else if(total === 5 || total === 6 || total === 7){
-                player.style.left = '-4%';
-                player.style.top = '19%';
-                player.style.transform = ''
-                player.style.right = '';
-            }
-             else if(total === 4) {
-                player.style.left = '50%';
-                player.style.transform = 'translateX(-50%)';
-                player.style.top = '-25px';
-                player.style.right = '';
-            }
-             else if(total === 3) {
-                player.style.right = '14%';
-                player.style.top = '0px';
-            }
-            break;
-        case 3:
-            if(total === 9) {
-                player.style.left = '0%';
-                player.style.top = '20%';
-            } else if(total === 8){
-                player.style.left = '5%';
-                player.style.top = '10%';
-            } 
-            else if(total === 7) {
-                player.style.left = '26%';
-                player.style.transform = 'translateX(-25%)';
-                player.style.top = '-5px';
-                player.style.right = '';
-            } 
-            else if(total === 6) {
-                player.style.left = '50%';
-                player.style.transform = 'translateX(-50%)';
-                player.style.top = '-25px';
-                player.style.right = '';
-            } 
-            else if(total === 5) {
-                player.style.right = '-4%';
-                player.style.top = '19%';
-                player.style.transform = ''
-            } 
-            else if(total === 4) {
-                player.style.right = '-10%';
-                player.style.top = '50%';
-                player.style.transform = 'translateY(-50%)'
-            }
-            break; 
-        case 4: 
-            if(total === 9){
-                player.style.left = '24%';
-                player.style.transform = '';
-                player.style.top = '0%';
-                player.style.right = '';
-            } else if (total === 8){
-                player.style.left = '50%';
-                player.style.transform = 'translateX(-50%)';
-                player.style.top = '-25px';
-                player.style.right = '';
-            } 
-            else if(total === 7){
-                player.style.right = '21%';
-                player.style.top = '-5px';
-                player.style.transform = '';
-            } 
-            else if(total === 6) {
-                player.style.right = '-4%';
-                player.style.top = '19%';
-                player.style.transform = ''
-            }
-            else if(total === 5){
-                player.style.right = '-4%';
-                player.style.bottom = '19%';
-                player.style.transform = ''
-            }
-            break;
-        case 5:
-            if(total === 9){
-                player.style.right = '25%';
-                player.style.top = '0%';
-                player.style.transform = ''
-            } else if(total === 8) {
-                player.style.right = '5%';
-                player.style.top = '10%';
-                player.style.transform = ''
-            } 
-            else if(total === 7){
-                player.style.right = '-4%';
-                player.style.top = '19%';
-                player.style.transform = ''
-            }
-            else if(total === 6){
-                player.style.right = '-4%';
-                player.style.bottom = '19%';
-                player.style.transform = ''
-            }
-            break;
-        case 6: 
-            if(total === 9){
-                player.style.transform = '';
-                player.style.right = '0%';
-                player.style.top = '20%';
-            } else if(total === 8){
-                player.style.transform = 'translateY(-50%)';
-                player.style.top = '50%';
-                player.style.right = '-10%'
-            }
-            else if( total === 7){
-                player.style.right = '-4%';
-                player.style.bottom = '19%';
-                player.style.transform = ''
-            }
-            break;
-        case 7: 
-            if(total === 9){
-                player.style.top = '50%';
-                player.style.right = '-10%';
-            }else if( total === 8){
-                player.style.right = '5%';
-                player.style.bottom = '10%';
-            }
-            break;
-        case 8: 
-            if(total === 9) {
-                player.style.right = '5%';
-                player.style.bottom = '10%';
-                player.style.transform = ''
+    const position = playerPositions[total]?.[index];
+    if(position){
+        for(const prop in position){
+            player.style[prop] = position[prop];
         }
-    } 
+    }
 }
 function createPlayer() {
     const avatar = randomAvatar();
