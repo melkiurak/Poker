0f76ad8ffd6224e9ec190b29717e56e29d89182b
melkiurak <melkiurak3209@gmail.com>
2025-06-04 13:47:54 +0300

    Add verificition form

--- scripts/game.js
+++ scripts/game.js
@@ -9,21 +9,37 @@ function optionsTexasHoldem() {
     const options = document.createElement('div')
     options.classList.add('game__container-options')
     options.innerHTML = `
-    <form class="options__form">
-        <div class="options__form-group">
+    <form class="options__form" id="gameForm">
+        <div class="options__form-group" id="formGroup">
             <label for="">Enter the name</label>
-            <input type="text">
+            <input type="text" id="playerName">
         </div>
         <div class="options__form-group">
             <label for="">Enter number of players</label>
-            <input type="number">
+            <input type="number" min="2" max="9" step="1" id="playersCount">
         </div>
-        <button class="options__form-start">Start of the game</button>
+        <button class="options__form-start" id="submit" type="submit">Start of the game</button>
     </form>
     `
-    titleGame.textContent = "Get Ready to Play — Texas Hold'em Poker"
+    titleGame.textContent = "Get Ready to Play — Texas Hold'em Poker";
     gameContainer.appendChild(options);
-
+    document.getElementById('gameForm').addEventListener('submit', verificationInput);
+}
+function verificationInput(e) {
+    e.preventDefault();
+    const checkName = /^[A-Za-zА-Яа-яЁё\s-]{2,30}$/;
+    const name = document.getElementById('playerName').value.trim();
+    const oldWarning = formGroup.querySelector('.form__warning');
+    if (oldWarning) {
+        oldWarning.remove();
+    }
+    if(!checkName.test(name.trim())){
+        const formGroup = document.getElementById("formGroup");
+        const warning = document.createElement('p')
+        warning.classList.add('form__warning')
+        warning.textContent = 'Enter a name from 2 to 30 characters. Only letters, spaces and hyphens are allowed.';
+        formGroup.appendChild(warning)
+    } 
 }
 
 texasHoldemBtn.addEventListener('click', () => {
