56332ec19cbf7301f9993bb9fef452ad32a009b0
melkiurak <melkiurak3209@gmail.com>
2025-06-14 11:20:18 +0300

    Fix width

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,6 +2,8 @@ import { texasHoldemState } from "./game.js";
 import { cardToString, colorSuit } from "./main.js";
 const deck = document.getElementById('deck');
 
+
+
 export function deckCards() {
     const suits = ['Diamonds', 'Clubs', 'Spades', 'Hearts'];
     const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
